47fdafd5e9017abe09e0117c3896d2232399d74b
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-03-31 10:22:53 -0400

    format funcitons

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
- /*******************
+/*******************
  * OUR HELPER CODE *
-*******************/
+ *******************/
 
 /*
  * Here we add the squares to the canvas dynamically.
  * You can mostly leave this section alone!
  * But if you want to change how wide the canvas is,
  * there are just two steps:
- * 
+ *
  * 1. Change the `gridWidth` value below.
  * 2. Change the `grid-template-rows` and
  * `grid-template-columns` to match.
@@ -18,9 +18,9 @@
 const gridWidth = 10;
 let count = 0;
 while (count < gridWidth * gridWidth) {
-  const canvas = document.querySelector('.canvas');
-  const div = document.createElement('div');
-  div.className = 'square color-5';
+  const canvas = document.querySelector(".canvas");
+  const div = document.createElement("div");
+  div.className = "square color-5";
   canvas.appendChild(div);
   count++;
 }
@@ -44,56 +44,55 @@ while (count < gridWidth * gridWidth) {
 
 /***********
  * QUERIES *
-***********/
+ ***********/
 let clickMouse = false;
 
 const getColor = (element) => {
   return element.classList[1];
-}
+};
 
 const clickSquare = (event) => {
   const square = event.target;
-  const brush = document.querySelector('.current-brush');
-      square.classList.replace(getColor(square)),(getColor(brush));
-      mouseDown = false;
-}
+  const brush = document.querySelector(".current-brush");
+  square.classList.replace(getColor(square)), getColor(brush);
+  mouseDown = false;
+};
 
 const mouseOverSquare = (event) => {
   if (mouseDown) {
     const square = event.target;
-    const brush = document.querySelector('.current-brush');
-    square.classList.replace(getColor(square),getColor(brush));  
+    const brush = document.querySelector(".current-brush");
+    square.classList.replace(getColor(square), getColor(brush));
   }
-}
+};
 
-const squares = document.querySelectorAll('.square') 
+const squares = document.querySelectorAll(".square");
 
 for (const square of squares) {
-  square.addEventListener('mouseenter', mouseOverSquare)
-  square.addEventListener('click', clickSquare)
+  square.addEventListener("mouseenter", mouseOverSquare);
+  square.addEventListener("click", clickSquare);
 }
 
-function clickPaletteColor (event) {
-  const brush = document.querySelector('.current-brush');
-  brush.classList.replace(getColor(brush),getColor(event.target));
+function clickPaletteColor(event) {
+  const brush = document.querySelector(".current-brush");
+  brush.classList.replace(getColor(brush), getColor(event.target));
 }
 
-const paletteColors = document.querySelectorAll('.palette-color');
+const paletteColors = document.querySelectorAll(".palette-color");
 
 for (const paletteColor of paletteColors) {
-  paletteColor.addEventListener('click', clickPaletteColor)
+  paletteColor.addEventListener("click", clickPaletteColor);
 }
 
-document.body.addEventListener('mousedown', () => {
+document.body.addEventListener("mousedown", () => {
   mouseDown = true;
-})
+});
 
-document.body.addEventListener('mouseup', () => {
+document.body.addEventListener("mouseup", () => {
   mouseDown = false;
-})
+});
 
-
-// // Alternative Version 
+// // Alternative Version
 // // Get an array of the brush color HTML elements
 // const brushColors = document.querySelectorAll('.palette-color');
 
@@ -109,7 +108,7 @@ document.body.addEventListener('mouseup', () => {
 //     const color = paletteColor.classList[1];
 
 //     // Set the classes of the currentColor HTML element
-//     currentColor.className = 'current-brush ' + color; 
+//     currentColor.className = 'current-brush ' + color;
 // }
 // // For each brush color HTML element...
 // for (const brushColor of brushColors) {
@@ -117,4 +116,3 @@ document.body.addEventListener('mouseup', () => {
 //   console.log('click happened');
 //     brushColor.addEventListener('click', handlePaletteClick);
 // }
-
